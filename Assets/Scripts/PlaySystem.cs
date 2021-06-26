0ebd3ea900a6068eb02fb5588ccb7da2124d4b4d
BakaWisdom <42656515+BakaWisdom@users.noreply.github.com>
2021-06-25 21:42:59 -0600

    started Finite State Machine
    https://www.youtube.com/watch?v=5PTd0WdKB-4
    following that tutorial to make a play system
    
    The idea behind this is that each robot will move forward and then raise the endmove event.  The state will be in charge of sending the start move and determine behavior after moving.
    
    This is mostly just setting up the skeleton though so this isn't implimented yet

---
+++ Assets/Scripts/PlaySystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySystem : MonoBehaviour
+{
+    internal GameState curState;
+
+    public void SetState(GameState newState)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
